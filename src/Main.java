b16ed2df5be4e6b4fee7921ff1b6a4b6680a4b04
Yazfish <sergei_kuzmin@mail.ru>
2024-10-23 17:06:11 +0300

    Start Commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+// Домашняя работа 8, массивы, урок 2.
+// Кузьмин Сергей
+// Java-разработчик IND 36.0
+//
+//
+public class Main {
+    public static void main(String[] args) {
+
+        }
+    }
